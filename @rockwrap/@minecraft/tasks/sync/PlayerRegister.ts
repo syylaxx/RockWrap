d4afd5cb059f09b0fdac59985edabf04c3234f29
m0lc14kk <m0lc14kk@outlook.com>
2024-09-10 14:12:31 +0200

    style: switched some task
    Switched task with registering players to arrow function.

---
+++ @rockwrap/@minecraft/tasks/sync/PlayerRegister.ts
@@ -0,0 +1,13 @@
+import { AfterEvents } from "../../Events/AfterEvents";
+import { DynamicPropertyManager } from "../../Managers/DynamicPropertyManager";
+
+const registerPlayers = (): void => {
+    AfterEvents.PlayerSpawned(({ player, playerJoined }) => {
+        if (!playerJoined) return;
+    
+        new DynamicPropertyManager(player.name).get(player.identifier)
+        new DynamicPropertyManager(player.identifier).get(player.name)
+    });
+};
+
+export { registerPlayers };
