5e3f6f4190529f56dd5eab5963526f83dc5fe862
syylaxx <agatobik1982@gmail.com>
2024-09-10 15:48:03 +0200

    feat: added main minecraft file for better import readability

---
+++ @rockwrap/minecraft.ts
@@ -0,0 +1,17 @@
+import { ItemStackDataBase } from "./@minecraft/DataBases/ItemStackDataBase"
+import { AfterEvents } from "./@minecraft/Events/AfterEvents"
+import { BeforeEvents } from "./@minecraft/Events/BeforeEvents"
+import { BlockManager } from "./@minecraft/Managers/BlockManager"
+import { DynamicPropertyManager } from "./@minecraft/Managers/DynamicPropertyManager"
+import { PlayerDataManager } from "./@minecraft/Managers/PlayerDataManager"
+import { PlayerManager } from "./@minecraft/Managers/PlayerManager"
+
+export {
+    ItemStackDataBase,
+    AfterEvents,
+    BeforeEvents,
+    BlockManager,
+    DynamicPropertyManager,
+    PlayerDataManager,
+    PlayerManager
+}
