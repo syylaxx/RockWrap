e3140456a09d4036e532aac6f2ed39e7c31ecafc
syylaxx <agatobik1982@gmail.com>
2024-09-10 17:07:37 +0200

    feat: changed codestyle

--- @rockwrap/minecraft.ts
+++ @rockwrap/minecraft.ts
@@ -1,7 +1,7 @@
-export { ItemStackDataBase } from "./@minecraft/DataBases/ItemStackDataBase"
-export { AfterEvents } from "./@minecraft/Events/AfterEvents"
-export { BeforeEvents } from "./@minecraft/Events/BeforeEvents"
-export { BlockManager } from "./@minecraft/Managers/BlockManager"
-export { DynamicPropertyManager } from "./@minecraft/Managers/DynamicPropertyManager"
-export { PlayerDataManager } from "./@minecraft/Managers/PlayerDataManager"
-export { PlayerManager } from "./@minecraft/Managers/PlayerManager"
+export { ItemStackDataBase } from "./@minecraft/DataBases/ItemStackDataBase";
+export { AfterEvents } from "./@minecraft/Events/AfterEvents";
+export { BeforeEvents } from "./@minecraft/Events/BeforeEvents";
+export { BlockManager } from "./@minecraft/Managers/BlockManager";
+export { DynamicPropertyManager } from "./@minecraft/Managers/DynamicPropertyManager";
+export { PlayerDataManager } from "./@minecraft/Managers/PlayerDataManager";
+export { PlayerManager } from "./@minecraft/Managers/PlayerManager";
