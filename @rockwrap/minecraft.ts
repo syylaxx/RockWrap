52df6561cb1175e4004aa0c057a0821bd51680f9
syylaxx <agatobik1982@gmail.com>
2024-09-10 15:51:37 +0200

    fix: deleted import and set export directly

--- @rockwrap/minecraft.ts
+++ @rockwrap/minecraft.ts
@@ -1,17 +1,7 @@
-import { ItemStackDataBase } from "./@minecraft/DataBases/ItemStackDataBase"
-import { AfterEvents } from "./@minecraft/Events/AfterEvents"
-import { BeforeEvents } from "./@minecraft/Events/BeforeEvents"
-import { BlockManager } from "./@minecraft/Managers/BlockManager"
-import { DynamicPropertyManager } from "./@minecraft/Managers/DynamicPropertyManager"
-import { PlayerDataManager } from "./@minecraft/Managers/PlayerDataManager"
-import { PlayerManager } from "./@minecraft/Managers/PlayerManager"
-
-export {
-    ItemStackDataBase,
-    AfterEvents,
-    BeforeEvents,
-    BlockManager,
-    DynamicPropertyManager,
-    PlayerDataManager,
-    PlayerManager
-}
+export { ItemStackDataBase } from "./@minecraft/DataBases/ItemStackDataBase"
+export { AfterEvents } from "./@minecraft/Events/AfterEvents"
+export { BeforeEvents } from "./@minecraft/Events/BeforeEvents"
+export { BlockManager } from "./@minecraft/Managers/BlockManager"
+export { DynamicPropertyManager } from "./@minecraft/Managers/DynamicPropertyManager"
+export { PlayerDataManager } from "./@minecraft/Managers/PlayerDataManager"
+export { PlayerManager } from "./@minecraft/Managers/PlayerManager"
