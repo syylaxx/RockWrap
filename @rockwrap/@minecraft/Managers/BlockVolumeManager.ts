5271d7ff6801f24aa84ffe29ad36b5f387cf9b23
m0lc14kk <m0lc14kk@outlook.com>
2024-09-24 16:44:23 +0200

    feat: some optimization and docs
    Optimised some code and documentation.

--- @rockwrap/@minecraft/Managers/BlockVolumeManager.ts
+++ @rockwrap/@minecraft/Managers/BlockVolumeManager.ts
@@ -1,22 +1,34 @@
 import { BlockVolume, Vector3 } from "@minecraft/server";
 
+/**
+ * Special class to work with block volumes.
+ */
 class BlockVolumeManager {
     private blockVolume: BlockVolume;
 
+    /**
+     * Declare a BlockVolume for our manager.
+     * @param volume 
+     */
     public constructor(volume: BlockVolume) {
         this.blockVolume = volume;
     };
 
-    public isInRange(location: Vector3): boolean {
+    /**
+     * Checks if location is in range of BlockVolume from constructor.
+     * @param location Location to check.
+     * @returns Returns a boolean whenever vector is inside a BlockVolume. Does not require privileges, like the default method.
+     */
+    public isInRange({ x, y, z }: Vector3): boolean {
         const { from, to } = this.blockVolume;
 
         return (
-            location.x >= Math.min(from.x, to.x) &&
-            location.x <= Math.max(from.x, to.x) &&
-            location.y >= Math.min(from.y, to.y) &&
-            location.y <= Math.max(from.y, to.y) &&
-            location.z >= Math.min(from.z, to.z) &&
-            location.z <= Math.max(from.z, to.z)
+            x >= Math.min(from.x, to.x) &&
+            x <= Math.max(from.x, to.x) &&
+            y >= Math.min(from.y, to.y) &&
+            y <= Math.max(from.y, to.y) &&
+            z >= Math.min(from.z, to.z) &&
+            z <= Math.max(from.z, to.z)
         );
     };
 };
--- @rockwrap/@minecraft/Managers/ConsoleManager.ts
+++ @rockwrap/@minecraft/Managers/ConsoleManager.ts
@@ -1,24 +1,48 @@
 import { RockWrap } from "../../config";
 
+/**
+ * Special class, that allows you to send messages with headers.
+ * @private
+ */
 class ConsoleManager {
     private constructor() {};
 
+    /**
+     * Sends a information in console.
+     * @param message Content of a information.
+     */
     public static info(message: any) {
         console.info(RockWrap.config["@console"].info + message);
     };
 
+    /**
+     * Sends a warning to console.
+     * @param message Content of a warning.
+     */
     public static warn(message: any) {
         console.warn(RockWrap.config["@console"].warn + message);
     };
 
+    /**
+     * Sends a debug message to console.
+     * @param message Content of a debug message.
+     */
     public static debug(message: any) {
         console.debug(RockWrap.config["@console"].debug + message);
     };
 
+    /**
+     * Sends an error message to console.
+     * @param message Content of an error.
+     */
     public static error(message: any) {
         console.error(RockWrap.config["@console"].error + message);
     };
 
+    /**
+     * Sends a basic messages, marked as log in console.
+     * @param message Content of a message.
+     */
     public static log(message: any) {
         console.log(RockWrap.config["@console"].log + message);
     };
