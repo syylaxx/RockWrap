f9890e89f13714e23d710353f0364e8aa4d5424d
m0lc14kk <m0lc14kk@outlook.com>
2024-09-24 16:37:41 +0200

    docs: documented ItemStackDatabase
    Documented ItemStackDatabase class.

--- @rockwrap/@minecraft/DataBases/ItemStackDataBase.ts
+++ @rockwrap/@minecraft/DataBases/ItemStackDataBase.ts
@@ -3,21 +3,53 @@ import { Enchantment, ItemStack } from "@minecraft/server";
 import { DynamicPropertyManager } from "../Managers/DynamicPropertyManager";
 
 interface ItemStackObject {
+    /**
+     * ID of an item.
+     */
     typeId: string,
+
+    /**
+     * Custom name of an item.
+     */
     nameTag: string,
+
+    /**
+     * Amount of an item.
+     */
     amount: number,
+
+    /**
+     * Durablity of an item.
+     */
     damage: number,
+
+    /**
+     * Lore of this item.
+     * Lore is a additional description for items.
+     */
     lore: string[],
+
+    /**
+     * Enchantments of this item.
+     */
     enchantments: Enchantment[]
 };
 
 class ItemStackDataBase {
     private identifier: string;
 
+    /**
+     * Creates an item database.
+     * @param identifier Identifier of database for this item.
+     */
     public constructor(identifier: string) {
         this.identifier = "ItemStackDataBase-" + identifier;
     };
 
+    /**
+     * Saves ItemStack to database.
+     * @param itemStack Item, which is going to be saved.
+     */
     public saveItemStack(itemStack: ItemStack) {
         const { typeId, nameTag, amount, getLore, getComponent } = itemStack;
         const { getEnchantments } = getComponent("enchantable");
@@ -34,6 +66,10 @@ class ItemStackDataBase {
         new DynamicPropertyManager(this.identifier).set(JSON.stringify(data));
     };
 
+    /**
+     * Gets saved ItemStack.
+     * @returns Item as ItemStack instance. 
+     */
     public getItemStack(): ItemStack {
         const data = JSON.parse(new DynamicPropertyManager(this.identifier).get() as string) as ItemStackObject;
         const { typeId, nameTag, amount, damage, lore, enchantments } = data;
@@ -47,6 +83,9 @@ class ItemStackDataBase {
         return itemStack;
     };
 
+    /**
+     * Resets item from database.
+     */
     public reset() {
         new DynamicPropertyManager(this.identifier).reset();
     };
--- @rockwrap/config.ts
+++ @rockwrap/config.ts
@@ -2,6 +2,10 @@ import { rockWrapConfiguration } from "./@config/RockWrapConfig";
 import { registerPlayers } from "./@minecraft/tasks/sync/PlayerRegister";
 
 class RockWrap {
+    /**
+     * Config of RockWrap wrapper.
+     * @readonly This property is in read-only mode. You can change values in config in `@rockwrap/@config/RockWrapConfig.ts`.
+     */
     public static readonly config = rockWrapConfiguration;
 
     private constructor() {};
@@ -9,7 +13,6 @@ class RockWrap {
     /**
      * This method is needed for some features, we recommend to execute it!
      */
-
     public static startup(): void {
         registerPlayers();
     };
