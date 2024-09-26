e4a5afbe45c90ae3008603f4759796d69cf1bd0e
m0lc14kk <m0lc14kk@outlook.com>
2024-09-26 16:16:48 +0200

    docs: documented PlayerDataManager
    Documented PlayerDataManager class.

--- @rockwrap/@minecraft/Managers/PlayerDataManager.ts
+++ @rockwrap/@minecraft/Managers/PlayerDataManager.ts
@@ -1,10 +1,14 @@
 import { Vector3 } from "@minecraft/server";
-
 import { DynamicPropertyManager } from "./DynamicPropertyManager";
+import type { DynamicPropertyValue } from "./types/DynamicPropertyValue";
 
 class PlayerDataManager {
     public readonly identifier: string;
 
+    /**
+     * Creates a new instance of player data manager.
+     * @param name Name of a player.
+     */
     public constructor(name: string) {
         const
             identifier = new DynamicPropertyManager(name).get() as string;
@@ -13,12 +17,23 @@ class PlayerDataManager {
             throw Error(`Player '${name}' was not registered.`);
 
         this.identifier = identifier;
-    }
+    };
 
-    public getData(identifier: string, replaceValue: string | number | boolean | Vector3 = undefined): string | number | boolean | Vector3 {
+    /**
+     * Gets a selected data.
+     * @param identifier Identifier, of key, which will be the source of data.
+     * @param replaceValue Default value.
+     * @returns Data or default value.
+     */
+    public getData(identifier: string, replaceValue: DynamicPropertyValue = undefined): DynamicPropertyValue {
         return new DynamicPropertyManager(this.identifier + ":" + identifier).get(replaceValue as any);
     };
 
+    /**
+     * Sets a data to selected identifier.
+     * @param identifier Identifier, which will be changed.
+     * @param value New data.
+     */
     public setData(identifier: string, value: string | number | boolean | Vector3): void {
         new DynamicPropertyManager(this.identifier + ":" + identifier).set(value);
     };
