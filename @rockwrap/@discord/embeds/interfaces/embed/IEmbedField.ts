e10c1bdac8a3f189a1247e39f84d0514daaa8b85
m0lc14kk <m0lc14kk@outlook.com>
2024-10-07 18:54:58 +0200

    docs: documented embeds
    Added documentation for Discord embeds.

--- @rockwrap/@discord/embeds/interfaces/embed/IEmbedField.ts
+++ @rockwrap/@discord/embeds/interfaces/embed/IEmbedField.ts
@@ -1,21 +1,40 @@
 import { EmbedContentTypes } from "../../types/EmbedContentTypes";
 
+/**
+ * Interface, which is a fixed structure of an embed field.
+ * @example
+ * ```ts
+ * import { EmbedWrapper } from "@rockwrap/discord";
+ * 
+ * const embed = new EmbedWrapper();
+ * embed.addFields({
+ *      name: "Name of a field.",
+ *      content: [
+ *          "Description of a field."
+ *      ],
+ * 
+ *      // If embed should be displayed in grid with other ones.
+ *      inline: false
+ * });
+ * ```
+ */
 interface IEmbedField {
     /**
      * Title of a field.
      */
-    name: string,
+    readonly name: string,
 
     /**
      * Content of a field.
      * If content is an array, null values are ignored.
      */
-    content: EmbedContentTypes | EmbedContentTypes[],
+    readonly content: EmbedContentTypes | EmbedContentTypes[],
 
     /**
-     * If field should be in-line with other ones. 
+     * If field should be in-line with other ones.
+     * @default false
      */
-    inline?: boolean
+    readonly inline?: boolean
 };
 
 export { IEmbedField };
