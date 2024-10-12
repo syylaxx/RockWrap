2e90c6b26cf5a108b859b604c0c9da47d1049595
TBroz15 <78770366+TBroz15@users.noreply.github.com>
2024-10-12 20:07:17 +0800

    feat(ItemStackDataBase): :sparkles: add more properties to be recognized by db

--- @rockwrap/@minecraft/DataBases/ItemStackDataBase.ts
+++ @rockwrap/@minecraft/DataBases/ItemStackDataBase.ts
@@ -1,6 +1,6 @@
 import { ItemStack } from "@minecraft/server";
 import { DynamicPropertyManager } from "../Managers/DynamicPropertyManager";
-import { ItemStackObject } from "./interfaces/ItemStackObject";
+import { ItemProperty, ItemStackObject } from "./interfaces/ItemStackObject";
 
 class ItemStackDataBase {
     private readonly identifier: string;
@@ -18,14 +18,37 @@ class ItemStackDataBase {
      * @param itemStack Item, which is going to be saved.
      */
     public saveItemStack(itemStack: ItemStack) {
-        const { typeId, nameTag, amount, getLore, getComponent } = itemStack;
+        const {
+          typeId,
+          amount,
+          nameTag,
+          getLore,
+          lockMode,
+          keepOnDeath,
+          getComponent,
+          getCanDestroy,
+          getCanPlaceOn,
+          getDynamicProperty,
+          getDynamicPropertyIds,
+        } = itemStack;
+
         const { getEnchantments } = getComponent("enchantable");
         const { damage } = getComponent("durability");
-        const data = {
-            typeId, nameTag, amount, damage,
+
+        const properties = getDynamicPropertyIds()
+                            .map((id): ItemProperty => ([
+                                id, 
+                                getDynamicProperty(id)
+                            ]))
+
+        const data: ItemStackObject = {
+            typeId, nameTag, amount, damage, lockMode, keepOnDeath, properties,
+
             lore: getLore(),
-            enchantments: getEnchantments()
-        };
+            canDestroy: getCanDestroy(),
+            canPlaceOn: getCanPlaceOn(),
+            enchantments: getEnchantments(),
+        }
 
         new DynamicPropertyManager(this.identifier).set(JSON.stringify(data));
     };
@@ -36,13 +59,31 @@ class ItemStackDataBase {
      */
     public getItemStack(): ItemStack {
         const data = JSON.parse(new DynamicPropertyManager(this.identifier).get() as string) as ItemStackObject;
-        const { typeId, nameTag, amount, damage, lore, enchantments } = data;
+        const { 
+            typeId, nameTag, amount, damage, lockMode, keepOnDeath, properties,
+            
+            lore,
+            canDestroy,
+            canPlaceOn,
+            enchantments
+        } = data;
+
         const itemStack = new ItemStack(typeId, amount);
 
         itemStack.nameTag = nameTag;
+        itemStack.lockMode = lockMode;
+        itemStack.keepOnDeath = keepOnDeath;
+
+        properties.forEach((property)=>{
+            itemStack.setDynamicProperty(...property);
+        })
+
         itemStack.setLore(lore);
         itemStack.getComponent("durability").damage = damage;
         itemStack.getComponent("enchantable").addEnchantments(enchantments);
+
+        itemStack.setCanDestroy(canDestroy);
+        itemStack.setCanPlaceOn(canPlaceOn);
 
         return itemStack;
     };
--- @rockwrap/@minecraft/DataBases/interfaces/ItemStackObject.ts
+++ @rockwrap/@minecraft/DataBases/interfaces/ItemStackObject.ts
@@ -1,4 +1,7 @@
-import { Enchantment } from "@minecraft/server";
+import { Enchantment, ItemLockMode, Vector3 } from "@minecraft/server";
+
+type ItemPropertyValue = string | number | boolean | Vector3;
+type ItemProperty = [string, ItemPropertyValue];
 
 interface ItemStackObject {
     /**
@@ -22,15 +25,42 @@ interface ItemStackObject {
     damage: number,
 
     /**
+     * Describes how an item can be moved within a container.
+     */
+    lockMode: ItemLockMode,
+
+    /**
+     * Describes if the item will be kept on death.
+     */
+    keepOnDeath: boolean,
+
+    /**
+     * The dynamic properties of an item.
+     *
+     * For every property, the first index is the key and the second is the value.
+     */
+    properties: ItemProperty[],
+
+    /**
      * Lore of this item.
      * Lore is a additional description for items.
      */
     lore: string[],
 
     /**
+     * The list of block types this item can break in Adventure mode.
+     */
+    canDestroy: string[],
+
+    /**
+     * The list of block types this item can be placed on in Adventure mode.
+     */
+    canPlaceOn: string[],
+
+    /**
      * Enchantments of this item.
      */
     enchantments: Enchantment[]
-};
+}
 
-export { ItemStackObject };
+export { ItemProperty, ItemStackObject };
